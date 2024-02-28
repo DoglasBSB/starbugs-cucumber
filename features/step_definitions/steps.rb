8cafa5cb10c54a2d60c394af3e4dcbb15878b51b
DoglasBSB <doglas_bsb@hotmail.com>
2024-02-28 08:22:30 -0300

    RePerforming checks with RSpec

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,8 +1,9 @@
 Quando('acesso a página principal da Starbugs') do
     visit 'https://starbugs.vercel.app/'
-    sleep 10
+    
 end
   
 Então('eu devo ver uma lista de cafés disponíveis') do
-    pending # Write code here that turns the phrase above into concrete actions
+    products = all('.coffee-item')
+    expect(products.size).to be > 0  
 end
